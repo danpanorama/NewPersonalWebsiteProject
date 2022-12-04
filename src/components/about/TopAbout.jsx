1045fa396cb476474669066792a4f0cf5e016627
danpanorama <62213659+danpanorama@users.noreply.github.com>
2022-12-04 16:01:16 +0200

    change content
    "

--- src/components/about/TopAbout.jsx
+++ src/components/about/TopAbout.jsx
@@ -11,7 +11,7 @@ function TopAbout(props) {
   
   
         <div className="headers">
-          <h1 className="headerBig" >i'm daniel Morcos and  <span  style={{ color: color.color }} >Web Developer</span> </h1>
+          <h1 className="headerBig" >I'm daniel Morcos and a  <span  style={{ color: color.color }} >Web Developer</span> </h1>
           <p className="peregraph">
           Developing and Designing Web from 2018. These days I am a freelance Full stack developer that codes my clients and friends ideas. I enjoy building websites, business cards and more.
 Let me Develop your idea!
